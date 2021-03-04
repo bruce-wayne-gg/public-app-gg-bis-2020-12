e986fe2841890ed8afd3132213c3566ec9c4ad76
Bruce Wayne <bruce@wayne-enterprises.tech>
2021-03-04 17:19:44 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import antigravity
 
-client_id=3MVG9YLyCqAG0j5xyPSKFLTEs2YMSrpD_dcj.SB7X2hn.akTf_ZU_Ox55_oO72uZUlwVrYPm66EIiahKm6h0c
-client_secret=0316681543149943177
+SEED=6668c5ee-30d8-4440-b180-2eae1af8da0f
 
 def main():
     antigravity.fly()
